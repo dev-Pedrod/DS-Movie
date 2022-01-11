893fe2076d2dd28544683d1229e7d4514690f461
dev-Pedrod <pedroolivxira@gmail.com>
2022-01-11 16:37:27 -0300

    Form layout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,11 +8,12 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/dev-Pedrod"></a>
+                    <a href="https://github.com/dev-Pedrod">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
                         <p className="dsmovie-contact-link">/devsuperior</p>
                     </div>
+                    </a>
                 </div>
             </nav>
         </header>
