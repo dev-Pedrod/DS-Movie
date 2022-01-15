964ff5d45784105bb8acfdb5550dfe5ca3a5be4a
dev-Pedrod <pedroolivxira@gmail.com>
2022-01-15 15:47:06 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,17 +1,28 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies`)
-        .then(Response => {
-        console.log(Response.data);
-    });
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+            .then(Response => {
+                    const data = Response.data as MoviePage;
+                    console.log(data);
+                setPageNumber(data.number)
+            });
+    }, []);
+
+    
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="coutaniner">
