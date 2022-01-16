ba46e75ab90f4357e0c655143bb8436a107b0304
dev-Pedrod <pedroolivxira@gmail.com>
2022-01-16 15:15:31 -0300

    github dev-Pedrod

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/dev-Pedrod">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
-                        <p className="dsmovie-contact-link">/devsuperior</p>
+                        <p className="dsmovie-contact-link">/dev-Pedrod</p>
                     </div>
                     </a>
                 </div>
